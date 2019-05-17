cc439c0e054aa00aab5046fddcb02691ca1176c9
yaogengzhu <455947455@qq.com>
2019-05-17 12:09:35 +0800

    city数据渲染完成

--- src/components/cityIndex/allCity.vue
+++ src/components/cityIndex/allCity.vue
@@ -1,89 +1,25 @@
 <template>
   <div class="info">
     <ul>
-      <li>
+      <li v-for="item in allCity" :key="item.id">
         <h2>
-          万达影院
+          {{ item.nm }}
           <span class="price">23元起</span>
         </h2>
         <div class="address">
           <p>
-            <span class="name">当阳市子龙路新时代文化广场四楼电影城</span>
-            <span class="distance">900km</span>
+            <span class="name">{{ item.addr }}</span>
+            <span class="distance">{{ item.distance }}</span>
           </p>
         </div>
         <div class="card">
-          <span>改签</span>
-          <span>退</span>
-          <span>折扣卡</span>
-        </div>
-      </li>
-      <li>
-        <h2>
-          万达影院
-          <span class="price">23元起</span>
-        </h2>
-        <div class="address">
-          <p>
-            <span class="name">当阳市子龙路新时代文化广场四楼电影城</span>
-            <span class="distance">900km</span>
-          </p>
-        </div>
-        <div class="card">
-          <span>改签</span>
-          <span>退</span>
-          <span>折扣卡</span>
-        </div>
-      </li>
-      <li>
-        <h2>
-          万达影院
-          <span class="price">23元起</span>
-        </h2>
-        <div class="address">
-          <p>
-            <span class="name">当阳市子龙路新时代文化广场四楼电影城</span>
-            <span class="distance">900km</span>
-          </p>
-        </div>
-        <div class="card">
-          <span>改签</span>
-          <span>退</span>
-          <span>折扣卡</span>
-        </div>
-      </li>
-      <li>
-        <h2>
-          万达影院
-          <span class="price">23元起</span>
-        </h2>
-        <div class="address">
-          <p>
-            <span class="name">当阳市子龙路新时代文化广场四楼电影城</span>
-            <span class="distance">900km</span>
-          </p>
-        </div>
-        <div class="card">
-          <span>改签</span>
-          <span>退</span>
-          <span>折扣卡</span>
-        </div>
-      </li>
-      <li>
-        <h2>
-          万达影院
-          <span class="price">23元起</span>
-        </h2>
-        <div class="address">
-          <p>
-            <span class="name">当阳市子龙路新时代文化广场四楼电影城</span>
-            <span class="distance">900km</span>
-          </p>
-        </div>
-        <div class="card">
-          <span>改签</span>
-          <span>退</span>
-          <span>折扣卡</span>
+          <!-- 处理标签  简便的处理，会导致vue有小小的警告-->
+          <span
+            v-for="( list,key) in item.tag"
+            v-if="list==1"
+            :class="key | colorFormat(key)"
+            :key="key"
+          >{{ key | tagFormat(key) }}</span>
         </div>
       </li>
     </ul>
@@ -91,7 +27,59 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      allCity: [] // 存放城市数据
+    };
+  },
+  mounted() {
+    // console.log('ok')
+    this.axios.get("/api/cinemaList?cityId=10").then(res => {
+      // console.log(res)
+      // 判断数据是否请求成功
+      if (res.status === 200) {
+        // 将数据赋值到存放的数据数组中
+        this.allCity = res.data.data.cinemas;
+      }
+    });
+  },
+  // 过滤器处理tag
+  filters: {
+    tagFormat(key) {
+      switch (key) {
+        case "allowRefund":
+          return "改签";
+        // break;
+        case "snack":
+          return "小吃";
+        // break;
+        case "endorse":
+          return "退";
+        // break;
+        case "sell":
+          return "折扣卡";
+        // break;
+      }
+    },
+    colorFormat(key){
+        switch (key) {
+        case "allowRefund":
+          return "green";
+        // break;
+        case "snack":
+          return "ol";
+        // break;
+        case "endorse":
+          return "green";
+        // break;
+        case "sell":
+          return "ol";
+        // break;
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -142,6 +130,13 @@ export default {};
           border: 1px solid green;
           font-size: 12px;
           margin-right: 10px;
+        }
+        .ol {
+          // color: gold;
+          border: 1px solid gold;
+        }
+        .green {
+          border: 1px solid green;
         }
       }
     }
